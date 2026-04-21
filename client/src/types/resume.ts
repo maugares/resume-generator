249f47a286252ea463c1e2321060b3076f6498f7
Mauro Garcia Esteban <mauro.garciaesteban@gmail.com>
2026-04-21 22:33:14 +0200

    Add type resume

---
+++ client/src/types/resume.ts
@@ -0,0 +1,10 @@
+// client/src/types/resume.ts
+export interface ResumeData {
+  name: string;
+  email: string;
+  address: string;
+  phone: string;
+  education: string;
+  experience: string;
+  skills: string;
+}
